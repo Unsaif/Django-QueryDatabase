d9338bb8f3488ccd342e7837c926fd56d748c37a
Unsaif <71923141+Unsaif@users.noreply.github.com>
2022-02-09 19:09:28 +0000

    Update settings.py
    amazon changes

--- queryDatabase/settings.py
+++ queryDatabase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l-%wd+-=@(0aj&4tb@%5i^va_cl$1jw#-qd*ik%bz^d1izozg*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -80,8 +80,8 @@ DATABASES = {
         'NAME': 'american_gut_database',
         'HOST': '127.0.0.1',
         'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': '72Un0saif96'
+        'USER': 'timhu',
+        'PASSWORD': 'password'
     }
 }
 
