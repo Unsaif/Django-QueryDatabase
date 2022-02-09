0191822635ad1162a9d0315a8557b7e1f9a003ea
Unsaif <71923141+Unsaif@users.noreply.github.com>
2022-02-09 19:18:35 +0000

    Update settings.py
    '*'

--- queryDatabase/settings.py
+++ queryDatabase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l-%wd+-=@(0aj&4tb@%5i^va_cl$1jw#-qd*ik%bz^d1izozg*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
